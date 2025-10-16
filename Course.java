a2dadad599c677a34ecb7babaf32184f69615237
KarandeepSingh258255 <karandeepkrish@gmail.com>
2025-10-16 10:50:45 -0400

    course

--- Course.java
+++ Course.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class Course {
     private Professor instructor;
     private CollegeStudent[] Liststudents;
@@ -38,8 +40,39 @@ public class Course {
 
     // no arg
 
+    Course(){
+        this.courseName = "";
+        this.Liststudents = new CollegeStudent[0];
+        for (int i = 0; i < Liststudents.length; i++) {
+            Liststudents[i] = new CollegeStudent(Liststudents[i]);
+        }
+        this.instructor = new Professor();
+    }
 
+    //fully-loaded
+
+    Course(Professor instructor, CollegeStudent[] Liststudents, String courseName){
+        this.instructor = new Professor();
+        this.courseName = courseName;
+        this.Liststudents = new CollegeStudent[0];
+        for (int i = 0; i < Liststudents.length; i++){
+            Liststudents[i] = new CollegeStudent(Liststudents[i]);
+        }
+    }
+
+    @Override
+    public String toString() {
+        String s = "";
+        s += "Course: " + this.courseName;
+        s += "\nProfessor: " + this.instructor;
+        s += "\nCollege Student Info: ";
+        for (int i = 0; i < Liststudents.length; i++){
+            s += this.Liststudents[i].toString() + "\n";
+        }
+        return s;
+    }
 }
+
 
 
 
