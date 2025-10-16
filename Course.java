54a3a5974f22151c310b04d6fa0dc3dfb4fd3691
KarandeepSingh258255 <karandeepkrish@gmail.com>
2025-10-16 11:58:55 -0400

    course fixes

--- Course.java
+++ Course.java
@@ -5,43 +5,9 @@ public class Course {
     private CollegeStudent[] Liststudents;
     private String courseName;
 
-    //set get
-
-
-    public Professor getInstructor() {
-        Professor copy = new Professor();
-        return copy;
-    }
-
-    public void setInstructor(Professor instructor) {
-        this.instructor = new Professor();
-    }
-
-    public Student[] getListstudents() {
-        Student[] copy = new Student[Liststudents.length];
-        for (int i = 0; i < Liststudents.length; i++) {
-            copy[i] = Liststudents[i];
-        }
-        return copy;
-    }
-
-    public void setListstudents(CollegeStudent[] liststudents) {
-        this.Liststudents = new CollegeStudent[Liststudents.length];
-        for (int i = 0; i < liststudents.length; i++){
-            Liststudents[i] = new CollegeStudent(Liststudents[i]);
-        }
-    }
-
-    public String getCourseName(){
-        return courseName;
-    }
-    public void setCourseName(String courseName){
-        this.courseName = courseName;
-    }
-
     // no arg
 
-    Course(){
+    Course() {
         this.courseName = "";
         this.Liststudents = new CollegeStudent[0];
         for (int i = 0; i < Liststudents.length; i++) {
@@ -52,14 +18,49 @@ public class Course {
 
     //fully-loaded
 
-    Course(Professor instructor, CollegeStudent[] Liststudents, String courseName){
+    Course(Professor instructor, CollegeStudent[] Liststudents, String courseName) {
         this.instructor = new Professor();
         this.courseName = courseName;
         this.Liststudents = new CollegeStudent[0];
+        for (int i = 0; i < Liststudents.length; i++) {
+            Liststudents[i] = new CollegeStudent(Liststudents[i]);
+        }
+    }
+
+    //set get
+
+    public Professor getInstructor() {
+        Professor copy = new Professor();
+        return copy;
+    }
+
+public void setInstructor(Professor instructor) {
+    this.instructor = new Professor();
+}
+
+public Student[] getListstudents() {
+    Student[] copy = new Student[Liststudents.length];
+    for (int i = 0; i < Liststudents.length; i++) {
+        copy[i] = Liststudents[i];
+    }
+    return copy;
+}
+
+    public void setListstudent(CollegeStudent[] Liststudents) {
+        this.Liststudents = new CollegeStudent[Liststudents.length];
         for (int i = 0; i < Liststudents.length; i++){
             Liststudents[i] = new CollegeStudent(Liststudents[i]);
         }
     }
+
+
+    public String getCourseName(){
+        return courseName;
+    }
+    public void setCourseName(String courseName){
+        this.courseName = courseName;
+    }
+
 
     @Override
     public String toString() {
